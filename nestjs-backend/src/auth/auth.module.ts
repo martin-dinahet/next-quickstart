ed5c6584861871e22fc06bacd77b8896696c5531
Martin <martin.dinahet@proton.me>
2025-04-06 01:19:53 +0200

    change: changed jwt lifetime from 60s to 1h

--- nestjs-backend/src/auth/auth.module.ts
+++ nestjs-backend/src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { jwtConstants } from './constants';
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({ global: true, secret: jwtConstants.secret, signOptions: { expiresIn: '60s' } }),
+    JwtModule.register({ global: true, secret: jwtConstants.secret, signOptions: { expiresIn: '1h' } }),
   ],
   providers: [AuthService],
   controllers: [AuthController],
