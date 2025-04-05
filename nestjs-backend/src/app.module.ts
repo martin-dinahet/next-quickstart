4f7e6bd1eaee76f60b88a3f11ed33f8f29e29757
Martin <martin.dinahet@proton.me>
2025-04-05 19:42:24 +0200

    update: added security protections

--- nestjs-backend/src/app.module.ts
+++ nestjs-backend/src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, UsersModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
