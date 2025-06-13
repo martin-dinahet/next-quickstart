6b46660ad64c434eb9737731f0b14e313fe78ebb
Martin <martin.dinahet@proton.me>
2025-06-13 13:09:07 +0200

    fixed server/client component issues & added prisma seeding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
-import { Todo } from "@prisma/client";
-import { getTodos } from "@/services/todos";
-import { use } from "react";
+"use server";
 
-const HomePage: React.FC = () => {
-  const todos = use(getTodos());
+import { TodoList } from "@/components/todo-list";
+import { getTodos } from "@/services/todos";
+
+const HomePage: React.FC = async () => {
+  const todos = await getTodos();
 
   return (
-    <div>
-      <h1>Hello, World!</h1>
-      {todos.map((todo: Todo) => (
-        <div key={todo.id}>{todo.title}</div>
-      ))}
+    <div className="flex min-h-screen w-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="text-2xl font-bold">Next Quickstart</h1>
+        <p>
+          Edit <code className="bg-gray-100 p-1">./src/app/page.tsx</code> to get started!
+        </p>
+        <TodoList todos={todos} />
+      </div>
     </div>
   );
 };
