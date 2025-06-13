9c3c9383f556b14d787c7adc42f28f1f37cf7c00
Martin <martin.dinahet@proton.me>
2025-06-13 17:53:10 +0200

    removed all files from main

--- src/app/api/todos/route.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
-
-export const GET = async () => {
-  const todos = await prisma.todo.findMany();
-  return NextResponse.json({ todos });
-};
--- src/services/todos.ts
+++
@@ -1,6 +0,0 @@
-export const getTodos = async () => {
-  const response = await fetch(`${process.env.API_URL}/todos`);
-  if (!response.ok) throw new Error("Failed to fetch todos");
-  const { todos } = await response.json();
-  return todos;
-};
